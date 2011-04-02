7748fa3c15772102a8306465e9043a0180071ae1
Kenneth Reitz <me@kennethreitz.com>
2011-04-02 18:23:38 -0400

    parse the xml, show dir

--- dashboard/core.py
+++ dashboard/core.py
@@ -13,7 +13,21 @@ from flask import Flask
 app = Flask(__name__)
 
 
+GH_CHANGELOG_URL = 'https://github.com/changelog.atom'
+
+import requests
+from lxml import objectify, etree
+
 
 @app.route('/')
 def index():
     return 'Hello, from flask!'
+
+
+@app.route('/changelog')
+def show_changelog():
+
+    r = requests.get(GH_CHANGELOG_URL)
+    tree = objectify.fromstring(r.content)
+
+    return str(dir(tree))
